6b4cb6011ebe726dbdb8cfb3528572625fc49ed1
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-13 13:13:14 +0100

    :sparkles: Add network call
    Fetch people from API and study endpoint response

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+
+const peopleApi = 'https://swapi.co/api/people/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [peopleFetched, setPeopleFetched] = useState([])
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get(peopleApi)
+    .then(response => {
+      console.log(response.data)
+      const characters = response.data.results;
+      setPeopleFetched(characters);
+    })
+    .catch(error => {
+      console.log(error);
+      
+    })
+  }, [])
 
   return (
     <div className="App">
