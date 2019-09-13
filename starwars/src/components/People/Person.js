109c564e321871e5d7c2615defab665e458ac259
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-13 14:17:07 +0100

    :lipstick: Complete Person.js
    Update styles, add color to eye property, add font family to name

--- starwars/src/components/People/Person.js
+++ starwars/src/components/People/Person.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import styled from styled;
+import styled from 'styled-components';
+import male from '../../images/man.svg'
+import female from '../../images/girl-avatar.svg'
 
-const StyledPerson = styled.div`
+
+
+function Person({person}){
+    const src = person.gender === 'male' ? male : female;
+
+    const StyledPerson = styled.div`
+    border: .2rem solid white;
+    border-radius: 3rem;
+    padding-top: 2.5rem;
+    width: 30%;
+    margin: 1.3rem 1rem;
+
+    img {
+        width: 85%;
+        height: 25rem;
+        border-radius: 2rem;
+        margin: 0 auto;
+    }
+
+    p {
+        color: white;
+    }
 
 `
 
-function Person(){
+    const EyeP = styled.p`
+        color: white;
+
+        span {
+            color: ${person.eye_color}
+        }
+    `
+
+    const Name = styled.p`
+        font-family: 'Tangerine', serif;
+        font-size: 4rem;
+    `
+    
     return(
         <StyledPerson>
-
+            <img src={src}/>
+            <Name>{person.name}</Name>
+            <div>
+                <p>Height: {person.height}</p>
+                <p>Mass: {person.mass}</p>
+            </div>
+            <EyeP>Eye color: <span >{person.eye_color}</span> </EyeP>
+            <p>Hair color: {person.hair_color}</p>
+            <p>Gender: {person.gender}</p>
         </StyledPerson>
     )
 }
