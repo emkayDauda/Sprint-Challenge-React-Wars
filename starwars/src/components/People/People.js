abde9701e22b9b31a6800fca90d5b48d2e320440
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-13 12:37:11 +0100

    :sparkles: Scaffold necessary components
    Create and export Person and People components and empty styling, also add axios and styled-components dependencies

---
+++ starwars/src/components/People/People.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled';
+
+const StyledPeople = styled.div`
+
+`;
+
+function People() {
+    return(
+        <StyledPeople>
+
+        </StyledPeople>
+    )
+}
+
+export default People;
---
+++ starwars/src/components/People/Person.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from styled;
+
+const StyledPerson = styled.div`
+
+`
+
+function Person(){
+    return(
+        <StyledPerson>
+
+        </StyledPerson>
+    )
+}
+
+export default Person;
