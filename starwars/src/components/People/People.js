e5a0f8ead246799442801c04c5ef851831b54882
Maaruf Dauda <mkdauda.work@gmail.com>
2019-09-13 14:16:01 +0100

    :lipstick: Add style to People.js
    Flex container and wrap it

--- starwars/src/components/People/People.js
+++ starwars/src/components/People/People.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import styled from 'styled';
+import styled from 'styled-components';
 
+import Person from './Person'
 const StyledPeople = styled.div`
-
+    display: flex;
+    flex-wrap: wrap;
 `;
 
-function People() {
+function People({people}) {
     return(
         <StyledPeople>
-
+            {people.map(character => <Person person={character}/>)}
         </StyledPeople>
     )
 }
